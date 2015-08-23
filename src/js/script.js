cc444f8ea57dc909efc5fc7dbd5af1976c7b16b8
Josh Cummings <hello@joshcummingsdesign.com>
2015-08-22 18:46:41 -0700

    Added slider control functionality.

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 // Variables
-var img_width;
-var currentImg = 0;
-var maxImages = 3;
-var speed = 500;
+var snazzyImgWidth;
+var snazzyCurrentImg = 0;
+var snazzyMaxImgs = 3;
+var snazzyScrollSpeed = 500;
+var snazzyNth = 1;
 var swipeOptions = {
     triggerOnTouchEnd: true,
     swipeStatus: swipeStatus,
@@ -11,8 +12,12 @@ var swipeOptions = {
 };
 
 // Cache the DOM
-var $slides = $(".snazzy-slider__slides");
-var $image = $slides.find(".snazzy-slider__image");
+var $snazzySlides = $(".snazzy-slider__slides");
+var $snazzyImg = $snazzySlides.find(".snazzy-slider__image");
+var $snazzyControls = $(".snazzy-controls");
+var $snazzyLftBtn = $snazzyControls.find(".snazzy-controls__left-btn");
+var $snazzyRtBtn = $snazzyControls.find(".snazzy-controls__right-btn");
+var $snazzyDots = $snazzyControls.find(".snazzy-controls__dot");
 
 // Functions
 /*Catch each phase of the swipe,
@@ -25,49 +30,115 @@ function swipeStatus(event, phase, direction, distance) {
   if (phase == "move" && (direction == "left" || direction == "right")) {
     var duration = 0;
     if (direction == "left") {
-      scrollImages((img_width * currentImg) + distance, duration);
+      snazzyScrollImgs((snazzyImgWidth * snazzyCurrentImg) + distance, duration);
     } else if (direction == "right") {
-      scrollImages((img_width * currentImg) - distance, duration);
+      snazzyScrollImgs((snazzyImgWidth * snazzyCurrentImg) - distance, duration);
     }
   } else if (phase == "cancel") {
-    scrollImages(img_width * currentImg, speed);
+    snazzyScrollImgs(snazzyImgWidth * snazzyCurrentImg, snazzyScrollSpeed);
   } else if (phase == "end") {
     if (direction == "right") {
-      previousImage();
+      snazzyPrevImg();
     } else if (direction == "left") {
-      nextImage();
+      snazzyNextImg();
     }
   }
 }
 
-function previousImage() {
-  currentImg = Math.max(currentImg - 1, 0);
-  scrollImages(img_width * currentImg, speed);
-}
-
-function nextImage() {
-  currentImg = Math.min(currentImg + 1, maxImages - 1);
-  scrollImages(img_width * currentImg, speed);
-}
-
-// Manually update the position of the $slides on drag for Snazzy Slider
-function scrollImages(distance, duration) {
-  $slides.css("transition-duration", (duration / 1000).toFixed(1) + "s");
+// Manually update the position of the slides on drag for Snazzy Slider
+function snazzyScrollImgs(distance, duration) {
+  $snazzySlides.css("transition-duration", (duration / 1000).toFixed(1) + "s");
   // Inverse the number we set in the css
   var value = (distance < 0 ? "" : "-") + Math.abs(distance).toString();
-  $slides.css("transform", "translate(" + value + "px,0)");
+  $snazzySlides.css("transform", "translate(" + value + "px,0)");
+}
+
+function snazzyHighlight() {
+  $snazzyDots.removeClass("snazzy-dot-highlighted");
+  $(".snazzy-controls__dot:nth-child(" + snazzyNth + ")").addClass("snazzy-dot-highlighted");
+}
+
+function snazzyPrevImg() {
+  snazzyCurrentImg = Math.max(snazzyCurrentImg - 1, 0);
+  snazzyScrollImgs(snazzyImgWidth * snazzyCurrentImg, snazzyScrollSpeed);
+  if (snazzyNth !== 1) {
+    snazzyNth--;
+    snazzyHighlight();  
+  }
+}
+
+function snazzyNextImg() {
+  snazzyCurrentImg = Math.min(snazzyCurrentImg + 1, snazzyMaxImgs - 1);
+  snazzyScrollImgs(snazzyImgWidth * snazzyCurrentImg, snazzyScrollSpeed);
+  if (snazzyNth !== $snazzyDots.length) {
+    snazzyNth++;
+    snazzyHighlight();
+  }
+}
+
+function snazzyLastImg() {
+  snazzyCurrentImg = snazzyMaxImgs - 1;
+  snazzyScrollImgs(snazzyImgWidth * snazzyCurrentImg, snazzyScrollSpeed);
+  snazzyNth = $snazzyDots.length;
+  snazzyHighlight();
+}
+
+function snazzyFirstImg() {
+  snazzyCurrentImg = 0;
+  snazzyScrollImgs(snazzyImgWidth * snazzyCurrentImg, snazzyScrollSpeed);
+  snazzyNth = 1;
+  snazzyHighlight();
 }
 
 // Initialize TouchSwipe
 $(function () {
-  $slides.swipe(swipeOptions);
+  $snazzySlides.swipe(swipeOptions);
 });
 
 // Set the Snazzy Slider image width on load and resize
 $(window).on("load resize",function(e){
   $screenWidth = $(window).width();
-  img_width = $screenWidth;
-  $image.width($screenWidth);
+  snazzyImgWidth = $screenWidth;
+  $snazzyImg.width($screenWidth);
   // Move the image back into position
-  scrollImages(img_width * currentImg, 0);
+  snazzyScrollImgs(snazzyImgWidth * snazzyCurrentImg, 0);
+});
+
+// On left button click
+$snazzyLftBtn.click(function() {
+  // If it's not the first image
+  if (snazzyNth !== 1) {
+    // Go to the previous image
+    snazzyPrevImg();
+  } else {
+    // Go to the last image
+    snazzyLastImg();
+  }
+});
+
+// On right button click
+$snazzyRtBtn.click(function() {
+  // If it's not the last image
+  if (snazzyNth !== $snazzyDots.length) {
+    // Go to the next image
+    snazzyNextImg();
+  } else {
+    // Go to the first image
+    snazzyFirstImg();
+  }
+});
+
+// On dot click
+$snazzyDots.click(function() {
+  // Highligth this dot
+  $snazzyDots.removeClass("snazzy-dot-highlighted");
+  $(this).addClass("snazzy-dot-highlighted");
+  // Go to nth image
+  snazzyCurrentImg = $(this).index();
+  snazzyScrollImgs(snazzyImgWidth * snazzyCurrentImg, snazzyScrollSpeed);
+  snazzyNth = snazzyCurrentImg + 1;
+});
+
+$(window).click(function(){
+  console.log(snazzyNth);
 });
